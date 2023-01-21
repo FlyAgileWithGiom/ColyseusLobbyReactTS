dd20594c390d9f1b557b830fc884f8d6b6b989b0
Guillaume Duquesnay <guillaume.duquesnay@gmail.com>
2023-01-21 08:30:41 +0000

    updating basic server with appropriate tsconfig and package.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,6 @@
-import { Server } from "colyseus";
+import { Server } from "colyseus"
+const port = parseInt(process.env.PORT, 10) || 3000
 
-const server = new Server({
-  server: process.env.SERVER || "localhost",
-  port: process.env.PORT || 2567,
-});
-
-server.onShutdown(() => {
-  console.log("Server is shutting down!");
-});
-
-server.listen();
+const gameServer = new Server()
+gameServer.listen(port)
+console.log(`[GameServer] Listening on Port: ${port}`)
